d5b692dab606067cb32cdb9803a414c3e781a690
Neetibut Vasinondha <62812751+neetibut@users.noreply.github.com>
2022-05-23 22:07:49 +0700

    web-navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+   <Navbar />
+   <div className="container">
+        <article>
+          <h1>What is Lorem Ipsum? </h1>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+        </article>
+        </div>
+ </div>
   );
 }
 
